69662c20ef2d9e9aacb7a4e08faef54ce06c86c2
Shreyansh <main.doga@gmail.com>
2026-05-12 23:26:15 +0530

    Initial commit

--- scratch_search.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('Invoice Details Report_Extract.csv', encoding='utf-8') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        if '12612980' in str(row) or '3424' in str(row):
-            print(row)
